ec79d2f30baeff5bb6f31552bd88527d1ae87284
Riocodex <onwukachibike@gmail.com>
2024-02-21 07:39:45 +0100

    created

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+
+import { NextPage } from "next";
+import { Container, Heading } from "@chakra-ui/react";
+
+const Home: NextPage = () => {
+  return (
+   <Container>
+    <Heading>ERC20 Staking App</Heading>
+   </Container>
+  );
+};
+
+export default Home;
